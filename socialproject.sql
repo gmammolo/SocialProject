60891e1f6deb2a686e841f1dba4561744a6542a0
gmammolo <gmammolo@gmail.com>
2015-04-13 16:34:25 +0200

    -add .sql database export

---
+++ socialproject.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generato il: Apr 13, 2015 alle 16:32
+-- Versione del server: 5.5.41-0ubuntu0.14.04.1
+-- Versione PHP: 5.5.9-1ubuntu4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `socialproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Friendship`
+--
+
+CREATE TABLE IF NOT EXISTS `Friendship` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `applicant` int(11) NOT NULL,
+  `requested` int(11) NOT NULL,
+  `blocked` bit(2) NOT NULL DEFAULT b'0',
+  `accepted` tinyint(1) NOT NULL DEFAULT '0',
+  `acceptedDate` date DEFAULT NULL,
+  `requestDate` date DEFAULT NULL,
+  `blockedDateapllicant` date DEFAULT NULL,
+  `blockedDaterequested` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `applicant` (`applicant`),
+  KEY `requested` (`requested`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Profile`
+--
+
+CREATE TABLE IF NOT EXISTS `Profile` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(64) NOT NULL,
+  `avatar` varchar(250) NOT NULL,
+  `residenza` varchar(250) NOT NULL,
+  `data` date NOT NULL,
+  `email` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `Profile`
+--
+
+INSERT INTO `Profile` (`id`, `nome`, `avatar`, `residenza`, `data`, `email`) VALUES
+(1, 'tester', '', 'fidati', '2015-03-04', 'gdftr@gfds.it'),
+(2, 'tester', '', 'prova', '2015-03-02', 'testo');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `User`
+--
+
+CREATE TABLE IF NOT EXISTS `User` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `verified` tinyint(1) NOT NULL,
+  `roles` varchar(250) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `profile` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `Friendship`
+--
+ALTER TABLE `Friendship`
+  ADD CONSTRAINT `Friendship_ibfk_1` FOREIGN KEY (`applicant`) REFERENCES `Profile` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Friendship_ibfk_2` FOREIGN KEY (`requested`) REFERENCES `Profile` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
